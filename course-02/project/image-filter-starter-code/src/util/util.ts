18c4d2479d027d9bc8ad8658f2e0a99afeee74ce
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-09-21 22:24:28 +0100

    used axios to fix image filter function

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import Jimp from "jimp";
+import axios from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,7 +12,13 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const imageBuffer = await axios({
+        method: "get",
+        url: inputURL,
+        responseType: "arraybuffer"
+      });
+      // .then(function ({data: imageBuffer}) {
+      const photo = await Jimp.read(imageBuffer.data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
